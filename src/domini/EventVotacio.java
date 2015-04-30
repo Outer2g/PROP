e9e6e29a05f5b47b540c17590f6f575de55656bc
Outer2g <aol.treballs@gmail.com>
2015-04-30 19:25:50 +0200

    EventVotacioDriver EventVotacio Final

--- src/domini/EventVotacio.java
+++ src/domini/EventVotacio.java
@@ -1,20 +1,22 @@
 package domini;
 
+import java.awt.List;
 import java.util.ArrayList;
 
 public class EventVotacio extends Event {
-	private ArrayList[] votacions;
+	private ArrayList<ArrayList<Integer>> votacions;
 	public EventVotacio(String tipusEvent, Data dataEvent, String nomEvent) {
 		super(tipusEvent, dataEvent, nomEvent);
 	}
 	public EventVotacio(Data dataEvent,String nomEvent){
 		super("Votacio",dataEvent,nomEvent);
-		votacions= new ArrayList[4];
+		votacions= new ArrayList<ArrayList<Integer>>(4);
+		for(int i=0;i<4;++i)votacions.add(new ArrayList<Integer>());
 	}
-	public void IntroduirDades(Integer diputat,int votacio){
-		votacions[votacio].add(diputat);
+	public void introduirDades(Integer diputat,int votacio){
+		votacions.get(votacio).add(diputat);
 	}
-	public ArrayList[] consultaVotacions(){
-		return votacions.clone();
+	public ArrayList<ArrayList<Integer>> consultaVotacions(){
+		return (ArrayList<ArrayList<Integer>>) votacions.clone();
 	}
 }
